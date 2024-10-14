4a486b5716e243d4f11c5efbaea3f786163a9a70
Mac <mac@Macs-MacBook-Pro.local>
2024-10-14 15:43:19 +0100

    more details on searchscreen

--- components/screen/HomeScreen.js
+++ components/screen/HomeScreen.js
@@ -6,6 +6,7 @@ import { styles } from '../../theme';
 import TrendingMovies from "../TrendingMovies"
 import MovieList from '../MovieList';
 import theme  from '../../theme';
+import { useNavigation } from '@react-navigation/native';
 
 
 
@@ -15,6 +16,7 @@ const HomeScreen = () => {
 const [trending, setTrending] = useState([1,2,3]);
 const [upcoming, setUpComing] = useState([1,2,3]);
 const [topRated, setTopRated] = useState([1,2,3]);
+const navigation = useNavigation();
 
   return (
     <View className=" bg-primary h-full" >
@@ -24,7 +26,9 @@ const [topRated, setTopRated] = useState([1,2,3]);
        <View className="flex-row justify-between item-center mx-4 " >
         <Bars3CenterLeftIcon size="30" color="white"/>
         <Text className="text-white text-3xl font-bold  "><Text style={{color: styles.text}} >M</Text>ovies</Text>
-         <TouchableOpacity>
+         <TouchableOpacity
+          onPress={()=> navigation.navigate('Search')}
+         >
           <MagnifyingGlassIcon size="30" strokeWidth={2} color="white"/>
          </TouchableOpacity>
        </View>
